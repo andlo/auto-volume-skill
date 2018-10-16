a44eed1c786312f8041f03f6a4708499e0197ae1
Andreas Lorensen <andlo@outlook.dk>
2018-10-16 23:23:43 +0200

    ...

--- __init__.py
+++ __init__.py
@@ -16,7 +16,8 @@ class AutoSetVolume(MycroftSkill):
         #self.filename = os.path.join(get_ipc_directory(), "mic_level")
         #self.level = 25
         #self.mixer = Mixer()
-        global autovolume = True
+        global autovolume 
+        autovolume = True
         
 
     @intent_file_handler('volume.set.auto.intent')
