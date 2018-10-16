15a9d70ecbccd993ae4cad43b6806d3d3f8717c4
Andreas Lorensen <andlo@outlook.dk>
2018-10-16 22:30:56 +0200

    ...

--- __init__.py
+++ __init__.py
@@ -13,9 +13,9 @@ import os.path
 class AutoSetVolume(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        self.filename = os.path.join(get_ipc_directory(), "mic_level")
-        self.level = 25
-        self.mixer = Mixer()
+        #self.filename = os.path.join(get_ipc_directory(), "mic_level")
+        #self.level = 25
+        #self.mixer = Mixer()
         auto_set_volume()
 
 
@@ -27,23 +27,24 @@ def auto_set_volume():
     global meter_cur
     global meter_thresh
     mixer = Mixer()
-    with io.open(os.path.join(get_ipc_directory(), "mic_level"), 'r') as fh:
-        fh.seek(0)
-        while True:
-            line = fh.readline()
-            if line == "":
-                break
+    while True:
+        with io.open(os.path.join(get_ipc_directory(), "mic_level"), 'r') as fh:
+            fh.seek(0)
+            while True:
+                line = fh.readline()
+                if line == "":
+                    break
 
-            # Just adjust meter settings
-            # Ex:Energy:  cur=4 thresh=1.5
-            parts = line.split("=")
-            meter_thresh = float(parts[-1])
-            meter_cur = float(parts[-2].split(" ")[0])
-            if int(meter_thresh) > 10:
-                mixer.setvolume(74)
-            if int(meter_thresh) < 10:
-                mixer.setvolume(25)
-            
+                # Just adjust meter settings
+                # Ex:Energy:  cur=4 thresh=1.5
+                parts = line.split("=")
+                meter_thresh = float(parts[-1])
+                meter_cur = float(parts[-2].split(" ")[0])
+                if int(meter_thresh) > 10:
+                    mixer.setvolume(74)
+                if int(meter_thresh) < 10:
+                    mixer.setvolume(25)
+                
 
         
         
