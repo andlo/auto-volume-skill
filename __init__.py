2aacd6a04236918797fb3671399192136da4ab56
Andreas Lorensen <andlo@outlook.dk>
2018-10-16 21:09:00 +0200

    .,,

--- __init__.py
+++ __init__.py
@@ -8,32 +8,14 @@ import os.path
    #   start_mic_monitor(os.path.join(get_ipc_directory(), "mic_level"))
 
 
-  def read_file_from(self, bytefrom):
-        global meter_cur
-        global meter_thresh
-
-        with io.open(self.filename, 'r') as fh:
-            fh.seek(bytefrom)
-            while True:
-                line = fh.readline()
-                if line == "":
-                    break
-
-                # Just adjust meter settings
-                # Ex:Energy:  cur=4 thresh=1.5
-                parts = line.split("=")
-                meter_thresh = float(parts[-1])
-                meter_cur = float(parts[-2].split(" ")[0])
-
-
-
-
+  
 class AutoSetVolume(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         self.filename=os.path.join(get_ipc_directory(), "mic_level")
         self.level = 25
         auto_set_volume(self)
+
 
     @intent_file_handler('volume.set.auto.intent')
     def handle_volume_set_auto(self, message):
