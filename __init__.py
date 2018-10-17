4d4744a66bb0a1f4e134848c35ed51ab3325e6b3
Andreas Lorensen <andlo@outlook.dk>
2018-10-17 17:36:20 +0200

    ...

--- __init__.py
+++ __init__.py
@@ -41,9 +41,9 @@ class AutoSetVolume(MycroftSkill):
                 meter_thresh = float(parts[-1])
                 meter_cur = float(parts[-2].split(" ")[0])
                 if int(meter_thresh) > 10:
-                    mixer.setvolume(74)
+                    self.mixer.setvolume(74)
                 if int(meter_thresh) < 10:
-                    mixer.setvolume(25)
+                    self.mixer.setvolume(25)
             
 
         
