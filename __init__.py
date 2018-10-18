1c4ef7911020699e09564093843adc596b98ada5
Andreas Lorensen <andlo@outlook.dk>
2018-10-18 18:28:11 +0200

    ...

--- __init__.py
+++ __init__.py
@@ -19,10 +19,10 @@ class AutoSetVolume(MycroftSkill):
         self.filename = os.path.join(get_ipc_directory(), "mic_level")
         self.mixer = Mixer()
         self.schedule_repeating_event(self.auto_set_volume, None,5, 'AutoSetVolume')
-        if self.settings.get('HighNoice') = None:
+        if self.settings.get('HighNoice') == None:
             self.settings['HighNooice'] = 7
         
-        if self.settings.get('LowNoice') = None:
+        if self.settings.get('LowNoice') == None:
             self.settings['LowNooice'] = 3
             
 
