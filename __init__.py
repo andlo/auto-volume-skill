f6147a31743879cf0e277e545214aca8ee7395fb
Andreas Lorensen <andlo@outlook.dk>
2018-10-19 18:55:46 +0200

    ...

--- __init__.py
+++ __init__.py
@@ -50,9 +50,8 @@ class AutoSetVolume(MycroftSkill):
 
                 # Ex:Energy:  cur=4 thresh=1.5
                 parts = line.split("=")
-                #meter_thresh = float(parts[-1])
-                meter_cur = float(parts[-2].split(" ")[0])
-                meter_thresh = meter_cur
+                meter_thresh = int(float(parts[-1]))
+                #meter_cur = float(parts[-2].split(" ")[0])
                 
                 # Store the thresh level
                 if self.settings.get('HighNoice') == None:
