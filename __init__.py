6480eb29dd9e4bd7e23cc0c61c4fa8f78286e6a3
Andreas Lorensen <andlo@outlook.dk>
2018-10-21 19:46:58 +0200

    ...

--- __init__.py
+++ __init__.py
@@ -67,8 +67,8 @@ class AutoSetVolume(MycroftSkill):
                     #meter_cur = float(parts[-2].split(" ")[0])
                     
                     self.log.info("Mesure mic: " + str(meter_thresh))
-                    self.log.info(str(self.meter_low))
-                    self.log.info(str(self.meter_high))
+                    self.log.info("Meter low: " + str(self.meter_low))
+                    self.log.info("Meter high: " + str(self.meter_high))
                     
                     self.meter_thresh_list.append(meter_thresh)
                     if len(self.meter_thresh_list) > 60:
