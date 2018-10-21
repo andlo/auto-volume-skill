c24870d5a504867f6630a9235ac50ff4089dd230
Andreas Lorensen <andlo@outlook.dk>
2018-10-21 20:19:26 +0200

    ...

--- __init__.py
+++ __init__.py
@@ -35,8 +35,8 @@ class AutoSetVolume(MycroftSkill):
                     self.handle_listener_ended)
 
         self.meter_thresh = 0 
-        self.meter_high = 1
-        self.meter_low = 1
+        self.meter_high = 5
+        self.meter_low = 5
         self.meter_thresh_list = []
 
     def handle_listener_started(self, message):  
