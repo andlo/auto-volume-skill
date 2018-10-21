76e8ad656ec62e558b33950c49134e09e7e53bab
Andreas Lorensen <andlo@outlook.dk>
2018-10-21 22:30:36 +0200

    ...

--- __init__.py
+++ __init__.py
@@ -104,12 +104,16 @@ class AutoSetVolume(MycroftSkill):
                     volume = self.settings.get('Low volume')
                 self.log.info("Mesure mic: " + str(self.meter_thresh) + 
                               " Setting volume to :" + str(volume) + "%" + 
-                              " from " + self.mixer.getvolume())
+                              " from " + str(self.mixer.getvolume()) + "%")
                 if not volume == None:  
                     self.mixer.setvolume(volume)
         else:
             self.log.info("Running initial messurement. " + str(len(self.meter_thresh_list)))
-            
+            self.log.info("Mesure mic: " + str(meter_thresh))
+            self.log.info("Meter low: " + str(self.meter_low) + " level: " + str( self.meter_low + ((30 * self.meter_low) / 100)))
+            self.log.info("Meter high: " + str(self.meter_high) + " level: " + str(self.meter_high - ((10 * self.meter_high) / 100)))
+            self.log.info("meter_thresh_list: " + str(len(self.meter_thresh_list)))  
+
 
  
             
