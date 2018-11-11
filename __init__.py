0c6531f75a9ac9e761c940ac112849c021029869
Andreas Lorensen <andlo@outlook.dk>
2018-11-11 15:16:42 +0000

    code clenup

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
 from mycroft.util import get_ipc_directory
-#from mycroft.util import wait_while_speaking
 from mycroft.audio import wait_while_speaking
 from mycroft.skills.audioservice import AudioService
 from alsaaudio import Mixer
@@ -9,30 +8,30 @@ import os
 import os.path
 import time
 
- 
+
 class AutoVolume(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-    
+
     def initialize(self):
         self.filename = os.path.join(get_ipc_directory(), "mic_level")
         self.audio_service = AudioService(self.bus)
         self.mixer = Mixer()
-        self.schedule_repeating_event(self.auto_set_volume, None,5, 'AutoVolume')
-        self.schedule_repeating_event(self.mesure_mic_thresh, None,1, 'AutoVolume_messure')
+        self.schedule_repeating_event(self.auto_set_volume, None, 5, 'AutoVolume')
+        self.schedule_repeating_event(self.mesure_mic_thresh, None, 1, 'AutoVolume_messure')
 
         self.autovolume = True
-        if self.settings.get('High volume') == None:
+        if self.settings.get('High volume') is None:
             self.settings['High volume'] = 75
-        if self.settings.get('Normal volume') == None:
+        if self.settings.get('Normal volume') is None:
             self.settings['Normal volume'] = 60
-        if self.settings.get('Low volume') == None:
+        if self.settings.get('Low volume') is None:
             self.settings['Low volume'] = 35
 
-        self.add_event('recognizer_loop:record_begin',  
-                    self.handle_listener_started)  
-        self.add_event('recognizer_loop:record_end',  
-                    self.handle_listener_ended)
+        self.add_event('recognizer_loop:record_begin',
+                       self.handle_listener_started)
+        self.add_event('recognizer_loop:record_end',
+                       self.handle_listener_ended)
 
         wait_while_speaking()
         with io.open(self.filename, 'r') as fh:
@@ -43,12 +42,12 @@ class AutoVolume(MycroftSkill):
                 # Ex:Energy:  cur=4 thresh=1.5
                 parts = line.split("=")
                 meter_thresh = float(parts[-1])
-        
-        if self.settings.get('Highest messurement') == None:
+
+        if self.settings.get('Highest messurement') is None:
             self.settings['Highest messurement'] = meter_thresh
-        if self.settings.get('Lowest messurement') == None:
+        if self.settings.get('Lowest messurement') is None:
             self.settings['Lowest messurement'] = meter_thresh
-        
+
         self.volume = int(self.settings.get('Low volume'))
         self.meter_thresh = 0
         self.meter_high = meter_thresh
@@ -56,18 +55,18 @@ class AutoVolume(MycroftSkill):
         self.meter_thresh_list = []
         self.meter_thresh_list.append(meter_thresh)
 
-    def handle_listener_started(self, message):  
+    def handle_listener_started(self, message):
         self.autovolume = False
 
-    def handle_listener_ended(self, message):  
+    def handle_listener_ended(self, message):
         time.sleep(5)
         self.autovolume = True
-                   
+
     @intent_file_handler('activate.intent')
     def handle_activate(self, message):
         self.autovolume = True
         self.speak_dialog("activate")
-        
+
     @intent_file_handler('deactivate.intent')
     def handle_deactivate(self, message):
         self.autovolume = False
@@ -84,13 +83,13 @@ class AutoVolume(MycroftSkill):
                     # Ex:Energy:  cur=4 thresh=1.5
                     parts = line.split("=")
                     meter_thresh = float(parts[-1])
-   
+
                     self.meter_thresh_list.append(meter_thresh)
                     if len(self.meter_thresh_list) > 120:
                         self.meter_thresh_list.pop(1)
-                    
-                    self.meter_thresh = sum(self.meter_thresh_list) / float(len(self.meter_thresh_list))  
-                                        
+
+                    self.meter_thresh = sum(self.meter_thresh_list) / float(len(self.meter_thresh_list))
+
                     if self.meter_thresh < self.settings.get('Lowest messurement'):
                         self.settings['Lowest messurement'] = self.meter_thresh
                     if self.meter_thresh > self.settings.get('Highest messurement'):
@@ -110,14 +109,14 @@ class AutoVolume(MycroftSkill):
             if self.meter_thresh < low_level:
                 volume = self.settings.get('Low volume')
 
-            if volume != self.volume and volume != None:  
+            if volume != self.volume and volume is not None:
                 self.mixer.setvolume(int(volume))
                 self.volume = volume
-                self.log.info("Mic thresh: " + str(self.meter_thresh) + 
-                            " Low level: " + str(low_level) +
-                            " High level: " + str(high_level))
-                self.log.info("Setting volume to :" + str(volume)  + "%")
-    
+                self.log.info("Mic thresh: " + str(self.meter_thresh) +
+                              " Low level: " + str(low_level) +
+                              " High level: " + str(high_level))
+                self.log.info("Setting volume to :" + str(volume) + "%")
+
+
 def create_skill():
     return AutoVolume()
-
